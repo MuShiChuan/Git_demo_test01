97041a0d4d0a775666d6101301cef9869271c9b9
MuShiChuan <2158229213@qq.com>
2020-06-28 14:07:31 +0800

    创建类文件

---
+++ com/git/demo/App.java
@@ -0,0 +1,11 @@
+package com.git.demo;
+import javax.swing.*;
+public class App{
+	public static void main(String[] args){
+		JFrame f = new JFrame();
+		f.setBounds(100,100,200,200);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+		System.out.println("JFrame");
+	}
+}
